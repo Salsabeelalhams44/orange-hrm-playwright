7577e743335f673313aa147a8f041281690d4487
Salsabeel Alhams <s.alhams545@gmail.com>
2026-03-14 22:36:18 +0200

    Apply suggestions from code review
    Co-authored-by: MalikMlitat <mlitat88@gmail.com>

--- tests/login.py
+++ tests/login.py
@@ -3,9 +3,9 @@ import os
 
 def login(page: Page) -> Page:
     """Login to OrangeHRM using environment secrets"""
-    USERNAME = os.getenv("ORANGEHRM_USERNAME")
-    PASSWORD = os.getenv("ORANGEHRM_PASSWORD")
-    BASE_URL = os.getenv("ORANGEHRM_BASE_URL")
+    username = os.getenv("ORANGEHRM_USERNAME")
+    password = os.getenv("ORANGEHRM_PASSWORD")
+    base_url = os.getenv("ORANGEHRM_BASE_URL")
 
     page.goto(f"{BASE_URL}/web/index.php/auth/login")
     page.get_by_role("textbox", name="Username").fill(USERNAME)
