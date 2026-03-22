5fdc8074c52352298c81709ef9f17547bf6708f2
Salsabeelalhams44 <s.alhams545@gmail.com>
2026-03-22 18:34:52 +0200

    feat: implement PIM page object and add employee navigation tests

---
+++ tests/e2e/test_pim.py
@@ -0,0 +1,15 @@
+import pytest
+from pages.pim_page import PimPage
+
+
+@pytest.mark.parametrize(
+    "selector_role, selector_name",
+    [
+        ("button", "Add"),
+        ("link", "Add Employee"),
+    ],
+)
+def test_navigate_to_add_employee_page_successfully(
+    pim_page: PimPage, selector_role, selector_name
+):
+    pim_page.navigate_to_add_employee(selector_role, selector_name)
