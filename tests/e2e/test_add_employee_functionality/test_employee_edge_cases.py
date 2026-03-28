b3511a047a42f0fefe04b9c488cefd238ebcbd86
Salsabeelalhams44 <s.alhams545@gmail.com>
2026-03-28 19:32:04 +0300

    fix: use fixed employee ID in duplicate test to reliably trigger duplicate error on CI

--- tests/e2e/test_add_employee_functionality/test_employee_edge_cases.py
+++ tests/e2e/test_add_employee_functionality/test_employee_edge_cases.py
@@ -3,10 +3,14 @@ from utils.helper import fill_basic_info
 
 
 def test_duplicate_employee_id(pim_page, add_employee_page):
+    duplicate_id = "99999"
+
     fill_basic_info(add_employee_page, "Duplicate", "ID")
 
-    emp_id = add_employee_page.get_employee_id()
+    add_employee_page.fill_employee_id(duplicate_id)
     add_employee_page.click_save()
+
+    add_employee_page.page.wait_for_url("**/pim/viewPersonalDetails/**", timeout=15000)
 
     pim_page.navigate_to_add_employee(selector_role="button", selector_name="Add")
     add_employee_page2 = AddEmployeePage(
@@ -14,7 +18,7 @@ def test_duplicate_employee_id(pim_page, add_employee_page):
     )  # create new AddEmployeePage instance
 
     fill_basic_info(add_employee_page2, "Duplicate", "ID2")
-    add_employee_page2.fill_employee_id(emp_id)
+    add_employee_page2.fill_employee_id(duplicate_id)
 
     add_employee_page2.click_save()
 
